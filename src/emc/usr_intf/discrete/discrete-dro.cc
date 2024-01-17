4592573042fdcf783311d964c560afb72dfb6049
Chad Woitas <cwoitas@rmd-engineering.com>
2024-01-16 21:29:47 -0600

    Start Drawing Table.. emcstatus and emcfile aren't loading

--- src/emc/usr_intf/discrete/discrete-dro.cc
+++ src/emc/usr_intf/discrete/discrete-dro.cc
@@ -17,21 +17,25 @@
 #include <curses.h>
 #include <unistd.h>
 
-// #include "config.h"
-// #include "emc.hh"
-// #include "emc_nml.hh"
 #include "emcIniFile.hh" // emcIniFile
-// #include "emcglb.h"		// EMC_NMLFILE, TRAJ_MAX_VELOCITY, etc.
-// #include "emccfg.h"		// DEFAULT_TRAJ_MAX_VELOCITY
-// #include "canon.hh"		// CANON_UNITS, CANON_UNITS_INCHES,MM,CM
-// #include "posemath.h"		// PM_POSE, TO_RAD
-// #include "rcs_print.hh"
-// #include "rcs.hh"
-// #include <rtapi_string.h>
-// #include "usrmotintf.h" // usrmotIniLoad, usrmotInit
+#include "canon.hh"		// CANON_UNITS, CANON_UNITS_INCHES,MM,CM
+#include "config.h"
+#include "emc.hh"		// EMC NML
+#include "emc_nml.hh"
+#include "emcglb.h"		// EMC_NMLFILE, TRAJ_MAX_VELOCITY, etc.
+#include "emccfg.h"		// DEFAULT_TRAJ_MAX_VELOCITY
+#include "inifile.hh"
+#include "kinematics.h"
+#include "posemath.h"		// PM_POSE, TO_RAD
+#include "rcs_print.hh"
+#include "rcs.hh"
+#include "rs274ngc.hh"
+#include "rs274ngc_interp.hh"
+#include "emc/usr_intf/shcom.hh"
+#include <rtapi_string.h>
 
 WINDOW *w_dro;
-WINDOW *w_test;
+
 uint16_t mid_top_col, mid_bot_col;
 uint16_t mid_left_lines, mid_right_lines;
 uint16_t max_h, max_w;
@@ -40,35 +44,134 @@ uint16_t max_h, max_w;
 int display_axis[] = {-1, -1, -1, -1, -1,
                       -1, -1, -1, -1, -1};
 
-int init_screen(){
-  //refresh();
-  printf("Max Height: %d\n", max_h);
-  printf("Max Width: %d\n", max_w);
+void init_screen(){
+  refresh();
   mid_top_col = max_w/2;
   mid_bot_col = max_w/2;
   mid_right_lines = (max_h/2)-1;
   mid_left_lines  = (max_h/2)-1;
 
-  w_dro = newwin(mid_right_lines, mid_top_col, 0, 0);
-  printf("tesT");
-  box(w_dro, 0, 0);
-  //nodelay(w_dro, TRUE);
+  w_dro = newwin(max_h, max_w, 0, 0);
 
-  // mvprintw(0,5  ,"Machine   Program   Offset");
-  // wrefresh(w_dro);
+  box(w_dro, 0, 0);
+  nodelay(w_dro, TRUE);
+
+  mvwprintw(w_dro, 0,5  ,"Machine   Program   Offset");
+  wrefresh(w_dro);
 }
 
 int init_machine(){
+
+//  iniLoad(emc_inifile);
+//  if (tryNml() != 0) {
+//    rcs_print_error("can't connect to emc\n");
+//    exit(1);
+//  }
+//
+
   EmcIniFile ini_file;
+  int num_axes = 0;
 
   ini_file.Open("/home/chad/linuxcnc/configs/sim-rmd-rtr/sim-rmd-rtr.ini");
   const char *coord = ini_file.Find("COORDINATES", "TRAJ");
-  printf("COORDINATES: %s\n", coord);
 
-  // Resume Thought here and parse the coordinates
-
+  if (coord) {
+    if(strchr(coord, 'x') || strchr(coord, 'X')) { display_axis[num_axes] = 1; num_axes++; }
+    if(strchr(coord, 'y') || strchr(coord, 'Y')) { display_axis[num_axes] = 2; num_axes++; }
+    if(strchr(coord, 'z') || strchr(coord, 'Z')) { display_axis[num_axes] = 3; num_axes++; }
+    if(strchr(coord, 'a') || strchr(coord, 'A')) { display_axis[num_axes] = 4; num_axes++; }
+    if(strchr(coord, 'b') || strchr(coord, 'B')) { display_axis[num_axes] = 5; num_axes++; }
+    if(strchr(coord, 'c') || strchr(coord, 'C')) { display_axis[num_axes] = 6; num_axes++; }
+    if(strchr(coord, 'u') || strchr(coord, 'U')) { display_axis[num_axes] = 7; num_axes++; }
+    if(strchr(coord, 'v') || strchr(coord, 'V')) { display_axis[num_axes] = 8; num_axes++; }
+    if(strchr(coord, 'w') || strchr(coord, 'W')) { display_axis[num_axes] = 9; num_axes++; }
+  }
 
   return 0;
+}
+
+void update_screen(){
+
+
+  for(int i=0; i<10;i++){
+    switch (display_axis[i]) {
+      case 1: {
+        mvwprintw(w_dro, i+1, 1,
+                  "X:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 2: {
+        mvwprintw(w_dro, i+1, 1,
+                  "Y:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 3: {
+        mvwprintw(w_dro, i+1, 1,
+                  "Z:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 4: {
+        mvwprintw(w_dro, i+1, 1,
+                  "A:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 5: {
+        mvwprintw(w_dro, i+1, 1,
+                  "B:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 6: {
+        mvwprintw(w_dro, i+1, 1,
+                  "C:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 7: {
+        mvwprintw(w_dro, i+1, 1,
+                  "U:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 8: {
+        mvwprintw(w_dro, i+1, 1,
+                  "V:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+      case 9: {
+        mvwprintw(w_dro, i+1, 1,
+                  "W:  %f  %f  %f",
+                  0.123,
+                  0.123,
+                  0.123);
+        break;
+      }
+    }
+
+  }
+
+  wrefresh(w_dro);
 }
 
 // <><><><><><><><><><><><><>
@@ -99,16 +202,15 @@ int main(int argc, char *argv[]) {
   keypad(stdscr, true);
   curs_set(0);
 
-//  char ch;
-//  bool run = true;
-//  while(run) {
-//    waddstr(w_dro, "DRO");
-//    wrefresh(w_dro);
-//    ch = getch();
-//    if (ch == 'q') {
-//      run = false;
-//    }
-//  }
+  char ch;
+  bool run = true;
+  while(run) {
+    update_screen();
+    ch = getch();
+    if (ch == 'q') {
+      run = false;
+    }
+  }
   // Screen Initialization
   // These Can not be abstracted from main
   endwin();
